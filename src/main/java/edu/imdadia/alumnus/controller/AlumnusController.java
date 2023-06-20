76047d55aa6b7f400a719e97f1a3fb5826e9fbc4
HammadAfzal812 <ibneafzal712@gmail.com>
2023-06-20 20:26:48 +0500

    update  alimnus

--- src/main/java/edu/imdadia/alumnus/controller/AlumnusController.java
+++ src/main/java/edu/imdadia/alumnus/controller/AlumnusController.java
@@ -431,5 +431,6 @@ public class AlumnusController implements Initializable {
         permanentAddress.setText(String.valueOf(alumnusEntity.getPermanentAddress()));
         graduationYear.setText(String.valueOf(alumnusEntity.getGraduationYear()));
         typeChoice.setValue(alumnusEntity.getType());
+        districtChoice.setValue(alumnusEntity.getType());
     }
 }
